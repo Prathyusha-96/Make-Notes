7dc9cace2d21a3bd00b89d31c5bc081bd0834262
Prathyusharam <prathyushaa1996@gmail.com>
2022-06-18 10:38:15 +0530

    feat: add homepage

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+import { HomePage } from "./HomePage/HomePage";
+import { Login } from "./Authentication/Login";
+import { Signup } from "./Authentication/Signup";
+export { HomePage,Login, Signup }
