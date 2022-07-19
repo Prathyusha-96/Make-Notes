63aac506c4d42c36adf436038702bd52a3e464f9
Prathyusharam <prathyushaa1996@gmail.com>
2022-07-19 11:25:23 +0530

    refactor: nav links

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,10 @@
 import "./home-page.css";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { LandingImage } from "../../assets";
+import { useAuth } from "../../contexts";
 
 const HomePage = () => {
+  const { auth, setAuth } = useAuth()
   return (
     <>
     <main className="page-container">
@@ -18,11 +20,21 @@ const HomePage = () => {
            Storing the data of your thoughts/ reminders in a place where we can access digitally
             through Make- Notes
           </div>
+          <div className="button-landing">
+            {auth.isAuth === true ? (
         <Link to="/addnote" >
               <button className="btn note-btn btn-primary">
                 Start Taking Notes
               </button>
             </Link>
+            ) : (
+              <Link to="/login" >
+              <button className="btn note-btn btn-primary">
+                Start Taking Notes
+              </button>
+            </Link>
+            )}
+            </div>
             </div>
         </div>
        
